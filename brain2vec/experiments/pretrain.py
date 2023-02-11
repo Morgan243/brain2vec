adfde3e82ee20ae11e5e965ffc492da31d8a8642
Morgan Stuart <morgansstuart243@gmail.com>
2023-02-11 14:40:04 -0500

    init check for existing results

--- brain2vec/experiments/pretrain.py
+++ brain2vec/experiments/pretrain.py
@@ -5,9 +5,11 @@ from mmz import models as bmm
 from brain2vec import datasets
 from brain2vec.datasets import harvard_sentences, northwestern_words
 from brain2vec.models import brain2vec
+from brain2vec.experiments import load_results_to_frame
 
 from simple_parsing import ArgumentParser, choice, subgroups
 from dataclasses import dataclass, field
+from typing import Optional
 import torch
 
 logger = utils.get_logger('semi_supervised')
@@ -42,7 +44,34 @@ class SemiSupervisedExperiment(mxp.Experiment):
          "dummy": SemisupervisedCodebookTaskOptions},
         default=SemisupervisedCodebookTaskOptions())
 
+    skip_if_results_exists_in: bool = None
+
+    def check_if_results_exist_in(self, check_existing_p):
+        import os
+        import pandas as pd
+        from glob import glob
+        check_existing_json_glob = os.path.join(check_existing_p, '*.json')
+        results_df = load_results_to_frame(check_existing_json_glob)
+
+        datasets_df = results_df.dataset_options.apply(pd.Series)
+
+        this_train_sets = set(harvard_sentences.HarvardSentences.make_tuples_from_sets_str(self.dataset.test_sets))
+
+        def check_for_match(train_sets_str):
+            train_sets = set(harvard_sentences.HarvardSentences.make_tuples_from_sets_str(train_sets_str))
+            return this_train_sets == train_sets
+
+        matching_train_set = datasets_df.train_set.apply(check_for_match)
+        if matching_train_set.any():
+            print(f"Found matching: {results_df[matching_train_set].to_dict()}")
+            return True
+        return False
+
     def run(self):
+        if self.skip_if_results_exists_in and self.check_if_results_exist_in(self.result_output.result_dir):
+            print("NOT RUNNING SINCE RESULTS ARE EXISTED")
+            return None, None
+
         # Reduce default test size for sklearn train/test split from 0.25 to 0.2
         dataset_map, dl_map, eval_dl_map = self.dataset.make_datasets_and_loaders(train_split_kws=dict(test_size=0.2))
         model, model_kws = self.model.make_model(dataset_map['train'])
